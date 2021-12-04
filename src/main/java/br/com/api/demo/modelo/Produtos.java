5201cba23cbc8b1e58bae1ecd60cdca5b372f205
Júlia <juliaspaderbianchi@gmail.com>
2021-12-04 14:00:51 -0300

    adicionei a classe modelo

---
+++ src/main/java/br/com/api/demo/modelo/Produtos.java
@@ -0,0 +1,63 @@
+package br.com.api.demo.modelo;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produtos {
+	
+	private String descricao;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Double preco;
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id, nome, preco);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produtos other = (Produtos) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(preco, other.preco);
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
