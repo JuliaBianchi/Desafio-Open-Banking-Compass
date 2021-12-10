0a662b7c79163b30dd84a990adf43ab247322d2a
Júlia Spader Bianchi <81650492+JuliaBianchi@users.noreply.github.com>
2021-12-10 14:13:51 -0300

    organizando os espaços

--- src/main/java/br/com/api/service/ProdutosService.java
+++ src/main/java/br/com/api/service/ProdutosService.java
@@ -14,36 +14,35 @@ public class ProdutosService {
 	@Autowired
 	private ProdutosRepository produtosRepository;
 	
-    public List<Produtos> findAll(){
-        return produtosRepository.findAll();
-    }
+	public List<Produtos> findAll(){
+        	return produtosRepository.findAll();
+    	}
    
-    public Produtos findById(Long id) {
-    	Optional<Produtos> produto = produtosRepository.findById(id);
-    	return produto.get();
-    }
+    	public Produtos findById(Long id) {
+		Optional<Produtos> produto = produtosRepository.findById(id);
+		return produto.get();
+    	}
     
-	public Produtos create(Produtos produto) {
+     	public Produtos create(Produtos produto) {
 		return produtosRepository.save(produto);
 	}
 	
-	public Produtos update(Produtos produto, Long id) {
+    	public Produtos update(Produtos produto, Long id) {
 		produto.setId(id);
 		return produtosRepository.save(produto);
 	}
 	
-    public void deleteById(Long id){
-        produtosRepository.deleteById(id);
-    }
+    	public void deleteById(Long id){
+		produtosRepository.deleteById(id);
+    	}
     
-    public List<Produtos> findBySearch(@RequestParam(required = false ,value = "q") 
-    String q, @RequestParam(required = false ,value = "minPrice") 
-    Double minPrice, @RequestParam(required = false , value = "maxPrice") 
-    Double maxPrice) {
-    	return produtosRepository.findBySearch(q, minPrice, maxPrice);
-    }
-
-	public Produtos save(Produtos produto) {
-		return produtosRepository.save(produto);
+    	public List<Produtos> findBySearch(@RequestParam(required = false ,value = "q") 
+    	String q, @RequestParam(required = false ,value = "minPrice") 
+    	Double minPrice, @RequestParam(required = false , value = "maxPrice") 
+   	 Double maxPrice) {
+    		return produtosRepository.findBySearch(q, minPrice, maxPrice);
+   	}
+    	public Produtos save(Produtos produto) {
+	    	return produtosRepository.save(produto);
 	}
 }
